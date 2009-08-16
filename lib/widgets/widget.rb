074cd425f4bc91d57c88f76fbc0ecfdd85bb3d94
Dominik Honnef <dominikho@gmx.net>
2009-08-16 15:44:00 +0200

    first relative drawing method

--- lib/widgets/widget.rb
+++ lib/widgets/widget.rb
@@ -83,6 +83,23 @@ module Widgets
       super if defined? super
     end
 
+    # draw relative to the widget
+    required_arguments :x1, :y1, :x2, :y2, :x3, :y3, :x4, :y4
+    default_arguments zorder: 1, mode: :default, color1: Gosu::Color.new(0xffffffff), color2: Gosu::Color.new(0xffffffff), color3: Gosu::Color.new(0xffffffff), color4: Gosu::Color.new(0xffffffff), color: nil
+    def draw_quad(args = { })
+      # TODO don't draw outside the container
+      if args[:color]
+        args[:color1], args[:color2], args[:color3], args[:color4] = args[:color], args[:color], args[:color], args[:color]
+      end
+      rel_x, rel_y = drawable_area[0]
+
+      @window.draw_quad(args[:x1]+rel_x, args[:y1]+rel_y, args[:color1],
+                        args[:x2]+rel_x, args[:y2]+rel_y, args[:color2],
+                        args[:x3]+rel_x, args[:y3]+rel_y, args[:color3],
+                        args[:x4]+rel_x, args[:y4]+rel_y, args[:color4], args[:zorder] + @zorder
+                        )
+    end
+
     def signal_connect(signal, &block)
       unless [:button_down, :button_up, :click].include? signal
         # TODO get real exception classes
