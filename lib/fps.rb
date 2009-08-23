e25530b16b5e8cc324535197c080f4995d114104
Dominik Honnef <dominikho@gmx.net>
2009-08-23 18:42:52 +0200

    more precice fps counter

--- lib/fps.rb
+++ lib/fps.rb
@@ -1,5 +1,17 @@
+class Float
+  alias_method :round_orig, :round
+  def round(n=0)
+    if n == 0
+      self.round_orig
+    else
+      (self * (10.0 ** n)).round_orig * (10.0 ** (-n))
+    end
+  end
+end
+
 class FPS
-  def initialize(init_time)
+  def initialize(init_time, precision=0)
+    @precision = precision
     @init_time = init_time
     @ticks = 0
     @fps = 0
@@ -7,14 +19,15 @@ class FPS
 
   def tick(current_time)
     @ticks += 1
+    @fps = (@ticks.to_f / (current_time - @init_time)) * 1000
     if current_time - @init_time >= 1000
-      @fps = @ticks
+      # @fps = @ticks
       @init_time = current_time
       @ticks = 0
     end
   end
 
   def to_i
-    @fps
+    @fps.round(@precision)
   end
 end
